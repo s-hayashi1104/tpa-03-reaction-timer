f7e4d7782e8fc63d9316f57367e16d82fbd744bb
s-hayashi1104 <shibahas.has@gmail.com>
2018-10-02 22:13:10 +0900

    ■Game.js ・不要なfor文削除 ・意味のないif文削除 ・コンソールに表示するカウントアップの判定が誤っていたため修正 ・２つ目のアクティブなセルをview側に通知できる用に変更

--- public/js/components/ReactionTimerGame.js
+++ public/js/components/ReactionTimerGame.js
@@ -27,35 +27,32 @@ class ReactionTimerGame {
   }
 
   triggerRandomCell() {
-    for (let i = 0; i < 2; i++) {
-      if (i === 0) {
-        const randomRowIndex = getRandomInt(0, NUM_ROWS);
-        const randomColIndex = getRandomInt(0, NUM_COLS);
-        this.activeCellRow = randomRowIndex;
-        this.activeCellCol = randomColIndex;
-        this.view.activateCell(randomRowIndex, randomColIndex);
-      } else {
-        const randomRowIndex2 = getRandomInt(0, NUM_ROWS);
-        const randomColIndex2 = getRandomInt(0, NUM_COLS);
-        this.activeCellRow2 = randomRowIndex2;
-        this.activeCellCol2 = randomColIndex2;
-        this.view.activateCell(randomRowIndex2, randomColIndex2);
-      }
-    }
+    const randomRowIndex = getRandomInt(0, NUM_ROWS);
+    const randomColIndex = getRandomInt(0, NUM_COLS);
+    this.activeCellRow = randomRowIndex;
+    this.activeCellCol = randomColIndex;
+    this.view.activateCell(randomRowIndex, randomColIndex);
+
+    const randomRowIndex2 = getRandomInt(0, NUM_ROWS);
+    const randomColIndex2 = getRandomInt(0, NUM_COLS);
+    this.activeCellRow2 = randomRowIndex2;
+    this.activeCellCol2 = randomColIndex2;
+    this.view.activateCell(randomRowIndex2, randomColIndex2);
   }
 
   handleActiveCellSelected() {
-    if (this.view.activeCellElRow === this.activeCellRow && this.view.activeCellElCol === this.activeCellCol && this.activeCellCol !== null && this.activeCellRow !== null) {
-      this.view.deactivateCell(this.activeCellRow, this.activeCellCol);
-      this.activeCellRow = null;
-      this.activeCellCol = null;
-      this.countUpValue += 1;
-    } else {
-      this.view.deactivateCell(this.activeCellRow2, this.activeCellCol2);
-      this.activeCellRow2 = null;
-      this.activeCellCol2 = null;
-      this.countUpValue += 1;
-    }
+    this.view.deactivateCell(this.activeCellRow, this.activeCellCol);
+    this.activeCellRow = null;
+    this.activeCellCol = null;
+    this.countUpValue += 1;
+    this.calculateTime();
+  }
+
+  handleActiveCellSelected2() {
+    this.view.deactivateCell(this.activeCellRow2, this.activeCellCol2);
+    this.activeCellRow2 = null;
+    this.activeCellCol2 = null;
+    this.countUpValue += 1;
     this.calculateTime();
   }
 
@@ -66,7 +63,7 @@ class ReactionTimerGame {
     }
     this.currentEndTime = new Date().getTime();
     console.log(`${numberOfClick} ${this.currentEndTime - this.currentStartTime}`);
-    if (this.countUpValue > 2) {
+    if (this.countUpValue === 2) {
       this.countUpValue = 0;
     }
   }
@@ -75,6 +72,7 @@ class ReactionTimerGame {
     this.view = new ReactionTimerGridView();
 
     this.view.registerActiveCellSelectedCallback(this.handleActiveCellSelected.bind(this));
+    this.view.registerActiveCellSelectedCallback2(this.handleActiveCellSelected2.bind(this));
     this.view.registerRoundStartCallback(this.handleRoundStart.bind(this));
 
     this.view.initDomAndListeners();
